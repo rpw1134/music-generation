c34f82a3c90a92852590233d87e5c15226980428
rpw1134 <ryanw1134@gmail.com>
2026-04-07 22:43:25 -0400

    saving numpy files. will use ragged array with lazy loading during batching

--- src/midi_gen/exploration/midi_test.py
+++ src/midi_gen/exploration/midi_test.py
@@ -24,20 +24,6 @@ def create_and_play_audio(filepath = "data/maestro-v3.0.0/2018/MIDI-Unprocessed_
 
     subprocess.run(['afplay', output_file])
 
-def midi_to_notes(filepath = "data/maestro-v3.0.0/2018/MIDI-Unprocessed_Chamber2_MID--AUDIO_09_R3_2018_wav--1.midi"):
-    midi_client = pretty_midi.PrettyMIDI(filepath)
-    notes = []
-    for instr in midi_client.instruments:
-        for note in instr.notes:
-            notes.append([note.start, note.end, note.pitch, note.velocity])
-    return notes
-
-def notes_to_vector(notes):
-    # row is a note, cols are the data points
-    return np.array(notes)
-
 
 if __name__ == "__main__":
-    notes = midi_to_notes()
-    vector = notes_to_vector(notes)
     print(vector)
