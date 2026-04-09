af52fbaba558be69e8f1caf8e0c827d0905014ed
rpw1134 <ryanw1134@gmail.com>
2026-04-08 22:08:48 -0400

    some comments on token scheme

--- src/midi_gen/data_management/parsing.py
+++ src/midi_gen/data_management/parsing.py
@@ -20,6 +20,12 @@ def file_path_to_vector(file_path):
 def save_vector_to_file(filepath, vector):
     np.save(filepath, vector)
 
+# will break into NOTE_ON PITCH, NOTE OFF PITCH, TIME_DELAY, VELOCITY tokens
+# TIME_DELAY is 10ms - 1s, 157 bins with: N bins from t_min to t_max, the bin edges are t_min * (t_max/t_min)^(i/N) for i in 0..N.
+# VELOCITY is 32 values. Bins
+# PITCH is 128 vals, same reason
+# 157 + 32 + 128 + 128 + 3 (PAD, EOS, BOS) = 448
+
 if __name__ == "__main__":
     v = file_path_to_vector("data/maestro-v3.0.0/2018/MIDI-Unprocessed_Chamber2_MID--AUDIO_09_R3_2018_wav--1.midi")
     save_vector_to_file("data/np/first.npy", v)
